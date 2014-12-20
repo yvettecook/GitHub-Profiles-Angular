60cf683fa4407bcf6aa627d0309331247e279cf3
Yvette <yvettecook@gmail.com>
2014-12-20 20:47:04 +0000

    Added functionality to search button on the back and front end. Currently hardcoded results

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,3 +1,20 @@
 githubUserSearch.controller('GitUserSearchController', function($scope) {
 
+
+  $scope.doSearch = function() {
+    $scope.searchResult = {
+      items: [{
+        "login": "yvettecook",
+        "avatarURL": "https://avatars2.githubusercontent.com/u/6195662?v=3&s=460",
+        "html_url": "https://github.com/yvettecook"
+      }, {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+
+      }]
+    }
+    console.log("Hello, world");
+  };
+
 });
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -9,9 +9,29 @@ describe("GitUserSearchController", function() {
       $scope: scope
     });
   }));
-  
+
   it('should initialise with an empty search term and result', function(){
     expect(scope.searchResult).toBeUndefined();
     expect(scope.searchTerm).toBeUndefined()
   })
+
+  describe("When searching for a user", function() {
+
+    var items = [{
+        "login": "yvettecook",
+        "avatarURL": "https://avatars2.githubusercontent.com/u/6195662?v=3&s=460",
+        "html_url": "https://github.com/yvettecook"
+      }, {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+    }];
+
+    it('should display search results', function() {
+      scope.searchTerm = "hello";
+      scope.doSearch();
+      expect(scope.searchResult.items).toEqual(items)
+    });
+  });
+
 });
